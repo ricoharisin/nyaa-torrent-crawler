ad7aa14c8686c5838a5989f1bdb6b52051034582
ricoharisin <rico.harisin@tokopedia.com>
2017-07-12 21:38:58 +0700

    add func for action

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/ricoharisin/nyaa-torrent-crawler/crawler"
 	"github.com/ricoharisin/nyaa-torrent-crawler/downloader"
@@ -9,6 +11,37 @@ import (
 )
 
 func main() {
+	args := os.Args
+	fmt.Println(len(args))
+	if len(args) < 2 {
+		showHelp()
+	} else {
+		switch args[1] {
+		case "subscribe":
+			subscribe(args[2], args[3])
+			break
+		case "crawl":
+			crawl()
+			break
+		default:
+			showHelp()
+			break
+		}
+	}
+}
+
+func subscribe(args1 string, args2 string) {
+	intargs, _ := strconv.Atoi(args2)
+	subscriber.InsertNewSubscribe(args1, intargs)
+}
+
+func showHelp() {
+	fmt.Println("usage: ")
+	fmt.Println("nyaa-torrent-crawler subscribe \"keyword\" start_episode_num")
+	fmt.Println("nyaa-torrent-crawler crawl")
+}
+
+func crawl() {
 	listSubscribe := subscriber.GetListSubscriber()
 	for i := range listSubscribe {
 		keyword, eps := subscriber.GetSubscribeInfo(i)
@@ -21,5 +54,4 @@ func main() {
 			}
 		}
 	}
-
 }
